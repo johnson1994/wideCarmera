e83cc4cecd802cb0ea3a5399f258b392955288b6
johnson1994 <starjohnson@sina.com>
2017-02-18 14:30:08 +0800

    modified config.cpp

---
+++ build/CMakeFiles/carmera.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/johnson/wideCarmera/caTask.cpp" "/home/johnson/wideCarmera/build/CMakeFiles/carmera.dir/caTask.cpp.o"
+  "/home/johnson/wideCarmera/combine.cpp" "/home/johnson/wideCarmera/build/CMakeFiles/carmera.dir/combine.cpp.o"
+  "/home/johnson/wideCarmera/config.cpp" "/home/johnson/wideCarmera/build/CMakeFiles/carmera.dir/config.cpp.o"
+  "/home/johnson/wideCarmera/main.cpp" "/home/johnson/wideCarmera/build/CMakeFiles/carmera.dir/main.cpp.o"
+  "/home/johnson/wideCarmera/mutipleCombine.cpp" "/home/johnson/wideCarmera/build/CMakeFiles/carmera.dir/mutipleCombine.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- config.h
+++ config.h
@@ -49,7 +49,8 @@ class Config{
 		VideoCapture* capture2;
 		/*摄像头3*/
 		VideoCapture* capture3;
-
+		
+		Mat edge1,edge2,edge3;
 };
 
 #endif
